a13e6641f43868ea5e04d803fc6a3d0fe8df601f
Joseph Ramli <joseph.ramli@binus.ac.id>
2019-05-11 18:50:13 +0700

    first
    first

--- LeagueItem.kt
+++
@@ -1,8 +0,0 @@
-package joseph.calcu.kotlinfirstsubmission
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class LeagueItem(val leaguename:String?,val image:Int?, val description:String?):Parcelable {
-}
---
+++ app/src/main/java/joseph/calcu/kotlinfirstsubmission/SecondActivity.kt
@@ -0,0 +1,12 @@
+package joseph.calcu.kotlinfirstsubmission
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
